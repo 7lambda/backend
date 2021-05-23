8d176d293567c6376d1d942f83d6bb43ae6220ab
alextran0899 <alextran@MacBook-Air.local>
2021-05-22 22:27:09 -0500

    lets see if this work

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ server.use(express.static(path.join(__dirname, 'client/build')))
 server.use(cors())
 server.use(express.json())
 
-server.get('/api', (req,res)=>{
-    res.json({message: 'api is working'})
-})
-server.use('/api/auth', authRouter);
-
 server.use('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
+
+server.get('/', (req,res)=>{
+    res.json({message: 'api is working'})
+})
+server.use('/api/auth', authRouter);
 
 server.use((err, req, res, next) =>{
     res.status(500).json({
