c5a719a10563148d94b64188c67cc7b1885d539f
alextran0899 <alextran@MacBook-Air.local>
2021-05-23 14:16:14 -0500

    converting msql to psql

--- api/Auth/auth-model.js
+++ api/Auth/auth-model.js
@@ -9,9 +9,8 @@ function findBy(filter) {
     return db('users').where(filter).first()
 }
 
-async function Add(data) { // done for you
-    const [userId] = await db('users').insert(data)
-    return db('users as u').where('user_id', userId).select('u.username', 'u.user_id')
+ function Add(data) { // done for you
+    return db('users').insert(data)
   }
 
   module.exports = {
