7cc4995034913f3cd28c743cf44c0e93b71f2250
alextran0899 <alextran@MacBook-Air.local>
2021-05-22 22:13:40 -0500

    fixed

---
+++ api/Auth/token-builder.js
@@ -0,0 +1,19 @@
+const jwt = require( 'jsonwebtoken')
+const {JWT_SECRET} = require('./secret')
+
+function tokenBuilder(user){
+    const payload = {
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1d',
+    }
+    const token = jwt.sign(
+        payload,
+        JWT_SECRET,
+        options
+    )
+    return token;
+}
+
+module.exports = tokenBuilder
