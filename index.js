5b2222ba2d35fcc9f2749df85af65ac2c2e31f05
alextran0899 <alextran@MacBook-Air.local>
2021-05-22 20:54:15 -0500

    4

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const cors = require('cors')
 
 const port = process.env.PORT || 9000
 
+server.use(express.static(path.join(__dirname, 'client/build')))
 server.use(cors())
 server.use(express.json())
 
 server.get('/api', (req,res)=>{
     res.json({message: 'api is working'})
+})
+
+server.use('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 server.use((err, req, res, next) =>{
