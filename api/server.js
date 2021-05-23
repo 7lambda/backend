70555f8836aaa4d6b40838f2579a5edc7e1bad95
alextran0899 <alextran@MacBook-Air.local>
2021-05-22 23:17:04 -0500

    removed react app

--- api/server.js
+++ api/server.js
@@ -4,7 +4,6 @@ const path = require('path')
 const cors = require('cors')
 const authRouter = require('./Auth/auth-router')
 
-server.use(express.static(path.join(__dirname, '../client/build')))
 server.use(cors())
 server.use(express.json())
 
@@ -12,10 +11,6 @@ server.get('/', (req,res)=>{
     res.json({message: 'api is working'})
 })
 server.use('/api/auth', authRouter);
-
-server.use('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-})
 
 server.use((err, req, res, next) =>{
     res.status(500).json({
