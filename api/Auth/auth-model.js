2be830e5bb2ac24444d9a08bf5224376515f896e
alextran0899 <alextran@MacBook-Air.local>
2021-05-23 14:20:20 -0500

    still trying

--- api/Auth/auth-model.js
+++ api/Auth/auth-model.js
@@ -10,7 +10,7 @@ function findBy(filter) {
 }
 
  function Add(data) { // done for you
-    return db('users').insert(data)
+    return db('users').insert(data,  ['user_id', 'username'])
   }
 
   module.exports = {
