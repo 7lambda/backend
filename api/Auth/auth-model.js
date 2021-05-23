f65e4226130c3bb69b9409e0fb88f5a4947454ad
alextran0899 <alextran@MacBook-Air.local>
2021-05-23 14:00:29 -0500

    jkoj

--- api/Auth/auth-model.js
+++ api/Auth/auth-model.js
@@ -11,7 +11,7 @@ function findBy(filter) {
 
 async function Add(data) { // done for you
     const [userId] = await db('users').insert(data)
-    return db('users as u').where('id', userId).select('u.username', 'u.id')
+    return db('users as u').where('user_id', userId).select('u.username', 'u.user_id')
   }
 
   module.exports = {
--- api/data/migrations/20210523184233_database.js
+++ api/data/migrations/20210523184233_database.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
     return knex.schema.createTable('users', users => {
-      users.increments();
+      users.increments('user_id');
       users.string('username', 255).notNullable().unique();
       users.string('password', 255).notNullable();
     });
