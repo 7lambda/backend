9ea462dd75b8860464c38e5f917257b949277393
alextran0899 <alextran@MacBook-Air.local>
2021-05-23 12:03:09 -0500

    moved everything index to serverjs

--- api/server.js
+++ api/server.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-
+const path = require('path')
 const server = express()
+
+server.use(express.static(path.join(__dirname, '../client/build')))
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-module.exports = server
+server.get('/api/*', (req,res)=>{
+    res.json({message: 'api is working'})
+})
+
+server.use('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+})
+
+
+server.use((err, req, res, next) =>{
+    res.status(500).json({
+        message:err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server;
