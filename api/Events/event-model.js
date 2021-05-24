75c21c877fdad2e47ef57c65f5cc2460848db1a9
alextran0899 <alextran@MacBook-Air.local>
2021-05-24 13:26:14 -0500

    added events router and model

--- api/Events/event-model.js
+++ api/Events/event-model.js
@@ -3,7 +3,24 @@ const db = require('../data/migrations/dbConfig')
 function getAll() {
     return db('events')
 }
+function getById(user_id) {
+    return db('events').where({user_id})
+}
 
-  module.exports = {
-      getAll,
-  }
+function insert(data) {
+    return db('events').insert(data, ['*'])
+}
+function updateevent(event_id, data) {
+    return db('events').where({event_id}).update(data, ['*'])
+}
+function nuked(event_id) {
+   return db('events').where({event_id}).del()
+}
+
+module.exports = {
+    getAll,
+    insert,
+    getById,
+    updateevent,
+    nuked
+}
--- api/Events/event-router.js
+++ api/Events/event-router.js
@@ -1,15 +1,43 @@
 const router = require('express').Router()
 const Event = require('./event-model')
 
-router.get('/getall', (req,res,next) =>{
-  Event.getAll()
-  .then(data =>{
-    res.json(data)
-  })
+router.get('/getall', (req, res, next) => {
+    Event.getAll()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(next)
+})
+router.get('/:user_id', (req, res, next) => {
+    Event.getById(req.params.user_id)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(next)
 })
 
-router.post('/:user_id', (req, res, next) => {
+router.put('/:event_id', (req, res, next) => {
+    console.log(req.params.event_id)
+    Event.updateevent(req.params.event_id,req.body)
+        .then(data => {
+            res.json(...data)
+        })
+        .catch(next) 
+});
+router.delete('/:event_id', (req, res, next) => {
+    Event.nuked(req.params.event_id)
+        .then(data => {
+            res.json({message:`${data} event deleted`})
+        })
+        .catch(next) 
+});
 
+router.post('/', (req, res, next) => {
+    Event.insert(req.body)
+        .then(data => {
+            res.json(...data)
+        })
+        .catch(next) 
 });
 
 module.exports = router;
